b99d16f45cd824a16857e96f9afbf2d0f9bc69de
Arpit Babbar <arpitbabbar01@gmail.com>
2023-07-16 01:55:11 +0530

    Added Memory buffer

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import streamlit as st
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain
+from langchain.memory import ConversationBufferMemory
 
 os.environ['OPENAI_API_KEY'] = apikey
 
@@ -22,12 +23,17 @@ script_template = PromptTemplate(
     template='write me a new youtube video script for my new video about {title}'
 )
 
+memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
+
 llm = OpenAI(temperature=0.5)
-title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title')
-script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='script')
+title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title', memory=memory)
+script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='script', memory=memory)
 sequential_chain = SequentialChain(chains=[title_chain, script_chain], verbose=True, input_variables=['topic'], output_variables=['title','script'])
 
 if prompt:
     resp = sequential_chain({'topic': prompt})
     st.write(resp['title'])
     st.write(resp['script'])
+
+    with st.expander('Message History'):
+        st.info(memory.buffer)
